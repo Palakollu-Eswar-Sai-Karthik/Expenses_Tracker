f58542e84845b2c6a04f82e1e21cebc25095e494
Palakollu-Eswar-Sai-Karthik <saikarthikpalakollu@gmail.com>
2026-09-09 15:30:43 +0530

    complete console code + json storage

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
-# List which contains user data
-expenses = []
+# import functions
+import json
+
+# File Name
+FILE_NAME = "storage.json"
+
+# Loading json data into code
+def load_expenses():
+    try:
+        with open(FILE_NAME,"r",encoding="utf-8") as file:
+            return json.load(file) 
+    except FileNotFoundError:
+        return []
+    except json.JSONDecodeError:
+        return []
+
+# Storing the data into json
+def save_expenses():
+    with open(FILE_NAME,"w",encoding="utf-8") as file:
+        json.dump(expenses,file,indent=4)
 
 # All Functions
 def design():
-    for i in range(17):
+    for _ in range(17):
         print("=",end="")
 
 def max_id()->int:
     max_value = 0
-    for i in expenses:
-        if i["id"] > max_value:
-            max_value = i["id"]
+    for expense in expenses:
+        if expense["id"] > max_value:
+            max_value = expense["id"]
     return max_value
         
-def AddExpense():
+def add_expense():
     user = {}
     id_value = max_id()
     user["id"] = id_value + 1
@@ -38,11 +56,12 @@ def AddExpense():
             else:
                 print("Description cannot be empty")
         else:
-            print("Category cannot be empty")    
+            print("Category cannot be empty")
     expenses.append(user)
+    save_expenses()
     
-def ViewExpenses():
-    if len(expenses)==0:
+def view_expenses():
+    if not expenses:
         print("No expense found!")
     else:
         print("\n")
@@ -51,10 +70,10 @@ def ViewExpenses():
         design()
         print("\n")
         print(f"{'ID':<5}{'Amount':<12}{'Category':<15}{'Description'}")
-        for i in expenses:
-            print(f"{i['id']:<5}{i['amount']:<12}{i['category']:<15}{i['description']}")
+        for expense in expenses:
+            print(f"{expense['id']:<5}{expense['amount']:<12}{expense['category']:<15}{expense['description']}")
    
-def UpdateExpense():
+def update_expense():
     while True:
         try:
             searchid = int(input("Enter the id of the expense: "))
@@ -89,46 +108,66 @@ def UpdateExpense():
             break
     else:
         print("Id not found")
+    save_expenses()
 
-def DeleteExpense():
+def delete_expense():
     while True:
         try:
             searchid = int(input("Enter the id of the expense: "))
             break
         except ValueError:
             print("Enter a valid ID!")
-    for i in expenses:
-        if i["id"]==searchid:
-            expenses.remove(i)
+    for expense in expenses:
+        if expense["id"]==searchid:
+            expenses.remove(expense)
             print("Expense deleted successfully")
             break
     else:
         print("Id not found!")
+    save_expenses()
 
-def ViewTotalExpenses():
-    if len(expenses)==0:
+def view_total_expenses():
+    if not expenses:
         print("No expense found!")
     else:
         totalexpenses=0
-        for i in expenses:
-            totalexpenses+=i["amount"]
+        for expense in expenses:
+            totalexpenses+= expense ["amount"]
         print(f"Total Expenses: {totalexpenses}")
 
-def FilterbyCategory():
+def filter_by_category():
     found = False
     search_category = input("Enter the category: ").lower()
-    for i in expenses:
-        if i["category"] == search_category:
+    for expense in expenses:
+        if expense["category"] == search_category:
             found = True
-            print(f"{i['id']}\t{i['amount']}\t\t{i['category']}\t\t{i['description']}")
+            print(f"{expense['id']:<5}{expense['amount']:<12}{expense['category']:<15}{expense['description']}")
     if not found:
         print("No category found!")
-    
-while(1):
+
+def search_expense():
+    found = False
+    while True:
+        search_word = input("Enter search keyword: ").lower()
+        if search_word:
+            break
+        else:
+            print("Search word cannot be empty")
+    for expense in expenses:
+        if search_word in expense["category"] or search_word in expense["description"]:
+            found = True
+            print(f"{expense['id']:<5}{expense['amount']:<12}{expense['category']:<15}{expense['description']}")
+    if not found:
+        print("No matching expense found!")
+        return
+# List which contains user data
+expenses = load_expenses()
+
+while True:
     design()
     print("\n Expense Tracker")
     design()
-    print("\n1. Add Expenses\n2. View Expenses\n3. Update Expense\n4. Delete Expense\n5. View Total Expenses\n6. Filter by Category\n7. Exit")
+    print("\n1. Add Expenses\n2. View Expenses\n3. Update Expense\n4. Delete Expense\n5. View Total Expenses\n6. Filter by Category\n7. Search Expense\n8. Exit")
     while True:
         try:
             choice = int(input("Enter your choice: "))
@@ -137,18 +176,20 @@ while(1):
             print("Enter a valid choice")
     match choice:
         case 1:
-            AddExpense()
+            add_expense()
         case 2:
-            ViewExpenses()
+            view_expenses()
         case 3:
-            UpdateExpense()
+            update_expense()
         case 4:
-            DeleteExpense()
+            delete_expense()
         case 5:
-            ViewTotalExpenses()
+            view_total_expenses()
         case 6:
-            FilterbyCategory()
+            filter_by_category()
         case 7:
+            search_expense()
+        case 8:
             print("Thankyou for using Expense Tracker!")
             break
         case _:
